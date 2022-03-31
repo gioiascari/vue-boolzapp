33d4d8eb3ef14a8682af766dcf168f0e2dc0d606
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-31 17:02:02 +0200

    inserimento traccia bonus

--- Milestone 4/js/main.js
+++ Milestone 4/js/main.js
@@ -2,7 +2,11 @@ console.log("Ok Vue Js");
 /*Milestone 4
 -Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
-“mar” rimangono solo Marco e Martina)*/
+“mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti*/
 const contacts = [
   {
     name: "Michele",
